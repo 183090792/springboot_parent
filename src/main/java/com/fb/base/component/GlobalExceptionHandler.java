e039985bd9978207bcb358ed48a4087b7f1101d1
123456 <183090792@qq.com>
2019-03-07 14:39:15 +0800

    controller 错误日志

--- src/main/java/com/fb/base/component/GlobalExceptionHandler.java
+++ src/main/java/com/fb/base/component/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.fb.base.component;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,13 +13,15 @@ import java.util.Map;
  * @create 2019/3/7 10:50
  **/
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     @ResponseBody
-    public Map<String, Object> exceptionHandler() {
+    public Map<String, Object> exceptionHandler(Exception e) {
+        log.debug("拦截的异常信息为==============>{}",e.getMessage());
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("errorCode", "101");
-        map.put("errorMsg", "系統错误!");
+        map.put("errorMsg", "系统错误!");
         return map;
     }
 }
